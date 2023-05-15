59201735ee5b0624d43660e3248f6b166524eff1
fiqriachmada <achmada1998@gmail.com>
2023-05-15 15:49:36 +0700

    fixed

--- app/api/v1/users/getUsers.js
+++ app/api/v1/users/getUsers.js
@@ -1,4 +1,4 @@
-const { Router } = require('express');
+const { Router, json } = require('express');
 const { connection } = require('../database/database');
 
 const getUsers = Router();
@@ -7,47 +7,34 @@ getUsers.get('/', async (req, res) => {
   const itemsPerPage = 10;
   const page = parseInt(req.query.page) || 1; // use query parameter or default to 1
   const offset = (page - 1) * itemsPerPage;
-  const userId = req.userId; // retrieve userId from the request object
 
   try {
     const countQuery = 'SELECT COUNT(*) FROM users'; // add WHERE clause to count only the logged-in user
-    // const countQuery = 'SELECT COUNT(*) FROM users'; // add WHERE clause to count only the logged-in user
-    // const { rows } = await (await connection()).query(countQuery, [userId]);
     const { rows: queryResult } = await (await connection()).query(countQuery);
-    console.log('rows', queryResult);
 
     const dataQuery = `SELECT * FROM users ORDER BY updated_at DESC
     LIMIT $1 OFFSET $2`; // add WHERE clause to retrieve data only for the logged-in user
-    // WHERE id = $1
-    // ORDER BY id DESC
 
     const { rows: dataResult } = await (
       await connection()
-    ).query(dataQuery, [
-      // userId,
-      itemsPerPage,
-      offset,
-    ]); // pass userId as a parameter to data query
-    console.log('dataResult', dataResult);
+    ).query(dataQuery, [itemsPerPage, offset]);
 
     const totalCount = queryResult[0].count;
 
     const totalPages = Math.ceil(totalCount / itemsPerPage);
 
-    console.log('totalCount', totalCount);
-    console.log('totalPages', totalPages);
+    const itemsOnLastPage = totalCount % itemsPerPage;
 
     const response = {
-      // rows: totalCount,
-      // rows,
       status: res.statusCode,
-      dataResult,
+      data: dataResult,
       meta: {
         pagination: {
           page: page,
           itemsPerPage: itemsPerPage,
           totalCount: totalCount,
           totalPages: totalPages,
+          itemsOnLastPage,
           links: {
             ...(page < totalPages
               ? {
@@ -73,7 +60,9 @@ getUsers.get('/', async (req, res) => {
     (await connection()).end();
   } catch (err) {
     console.log(err);
-    res.status(500).json({ message: 'Error retrieving users' });
+    res
+      .status(500)
+      .json({ message: 'Error retrieving users', error: JSON.stringify(err) });
   }
 });
 
