8be9377683626869cc043815c2b170e4540f1f59
fiqriachmada <achmada1998@gmail.com>
2023-05-15 15:37:48 +0700

    new

--- app/api/v1/users/getUsers.js
+++ app/api/v1/users/getUsers.js
@@ -4,31 +4,44 @@ const { connection } = require('../database/database');
 const getUsers = Router();
 
 getUsers.get('/', async (req, res) => {
+  const itemsPerPage = 10;
+  const page = parseInt(req.query.page) || 1; // use query parameter or default to 1
+  const offset = (page - 1) * itemsPerPage;
+  const userId = req.userId; // retrieve userId from the request object
+
   try {
-    const itemsPerPage = 10;
-    const page = parseInt(req.query.page) || 1; // use query parameter or default to 1
-    const offset = (page - 1) * itemsPerPage;
-    const userId = req.userId; // retrieve userId from the request object
+    const countQuery = 'SELECT COUNT(*) FROM users'; // add WHERE clause to count only the logged-in user
+    // const countQuery = 'SELECT COUNT(*) FROM users'; // add WHERE clause to count only the logged-in user
+    // const { rows } = await (await connection()).query(countQuery, [userId]);
+    const { rows: queryResult } = await (await connection()).query(countQuery);
+    console.log('rows', queryResult);
 
-    const countQuery = 'SELECT COUNT(*) FROM users WHERE id = $1'; // add WHERE clause to count only the logged-in user
-    const dataQuery = `SELECT * FROM users WHERE id = $1 ORDER BY id DESC LIMIT ${itemsPerPage} OFFSET ${offset}`; // add WHERE clause to retrieve data only for the logged-in user
+    const dataQuery = `SELECT * FROM users ORDER BY updated_at DESC
+    LIMIT $1 OFFSET $2`; // add WHERE clause to retrieve data only for the logged-in user
+    // WHERE id = $1
+    // ORDER BY id DESC
 
-    const countResult = await (await connection()).query(countQuery, [userId]);
     const { rows: dataResult } = await (
       await connection()
-    ).query(dataQuery, [userId]); // pass userId as a parameter to data query
+    ).query(dataQuery, [
+      // userId,
+      itemsPerPage,
+      offset,
+    ]); // pass userId as a parameter to data query
+    console.log('dataResult', dataResult);
 
-    const totalCount = countResult.rowCount[0];
+    const totalCount = queryResult[0].count;
+
     const totalPages = Math.ceil(totalCount / itemsPerPage);
 
-    console.log('countResult', countResult);
-    console.log('dataResult', dataResult);
     console.log('totalCount', totalCount);
     console.log('totalPages', totalPages);
 
     const response = {
+      // rows: totalCount,
+      // rows,
       status: res.statusCode,
-      data: dataResult,
+      dataResult,
       meta: {
         pagination: {
           page: page,
@@ -60,7 +73,7 @@ getUsers.get('/', async (req, res) => {
     (await connection()).end();
   } catch (err) {
     console.log(err);
-    res.status(500).res('Error retrieving users');
+    res.status(500).json({ message: 'Error retrieving users' });
   }
 });
 
