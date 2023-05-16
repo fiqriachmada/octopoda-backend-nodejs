df18b468b0d0e3422fdfe89c3729f8c284daa01a
fiqriachmada <achmada1998@gmail.com>
2023-05-16 12:20:28 +0700

    new push

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 
 const bodyParser = require('body-parser');
 const getUsers = require('./app/api/v1/users/getUsers');
-const { connection } = require('./app/api/v1/database/database');
+const { getClient, connection } = require('./app/api/v1/database/database');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -15,11 +15,26 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.get('/', async (req, res) => {
-  const { rows: connected } = await (await connection()).query('SELECT NOW()');
-  console.log('connected[0].now', connected[0].now);
-  const response = [{ version: '1', time: connected[0].now }];
-  res.json(response[0]);
+  // const client = getClient();
+  // try {
+  //   await client.connect(); // Connect to the database
+  const result = await (await connection()).query('SELECT * FROM Users'); // Execute your query
+  console.log(result.rows); // Log the results
+  res.json({ status: 'ok', result });
+  // } catch (error) {
+  //   console.error(error);
+  //   res.status(500).json({ error: 'Internal server error' });
+  // } finally {
+  //   await client.end(); // Disconnect from the database
+  // }
 });
+
+// app.get('/', async (req, res) => {
+//   const { rows: connected } = await (await connection()).query('SELECT NOW()');
+//   console.log('connected[0].now', connected[0].now);
+//   const response = [{ version: '1', time: connected[0].now }];
+//   res.json(response[0]);
+// });
 
 app.use('/users', getUsers);
 
--- app/api/v1/database/database.js
+++ app/api/v1/database/database.js
@@ -1,63 +1,25 @@
-var pg = require('pg');
+const pg = require('pg');
 const dotenv = require('dotenv');
 const fs = require('fs');
 
 dotenv.config();
 
-const databaseHost = process.env.DATABASE_HOST;
-const databasePort = process.env.DATABASE_PORT;
-const databaseName = process.env.DATABASE_NAME;
-const databaseUser = process.env.DATABASE_USER;
-const databasePassword = process.env.DATABASE_PASSWORD;
+const databaseUrl = process.env.DATABASE_URL;
 
-const config = new pg.Pool({
-  host: databaseHost,
-  port: databasePort,
-  database: databaseName,
-  user: databaseUser,
-  password: databasePassword,
-  ssl: {
-    rejectUnauthorized: true,
-    ca: fs.readFileSync('./root.crt').toString(),
-  },
-  connectionTimeoutMillis: 5000000,
-});
-
-let countdown = 5000000 / 1000;
-
-const connection = async () => {
-  try {
-    const pool = await getConnection();
-    console.log('Successfully connected to database!');
-    return pool;
-  } catch (err) {
-    console.error('Error connecting to database:', err);
-    throw new Error('Unable to establish a connection to the database');
-  }
+const sslConfig = {
+  rejectUnauthorized: false,
 };
 
+const connection = async () => {
+  return await getClient();
+};
 
-async function getConnection() {
-  let intervalId = setInterval(() => {
-    countdown--;
-    console.log(
-      `>>>> Connecting to YugabyteDB! Timeout in ${countdown} seconds...`
-    );
-  }, 1000);
-  console.log('>>>> Connecting to YugabyteDB!');
-  intervalId;
-  try {
-    const connected = await config.connect();
-    console.log('>>>> Connected to YugabyteDB! with Host ' + databaseHost);
-    clearInterval(intervalId);
-    return connected;
-  } catch (err) {
-    console.log(
-      '>>>> Connection Failed to YugabyteDB! with Host ' + databaseHost,
-      'Error: '
-    );
-    console.log(err);
-  }
+function getClient() {
+  const connect = new pg.Client({
+    connectionString: databaseUrl,
+    ssl: sslConfig,
+  });
+  return connect.connect().then(() => connect);
 }
 
 module.exports = { connection };
