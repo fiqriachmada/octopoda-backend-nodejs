930e1a7ed3ca4fba7ed62131fac727ae5febabc2
fiqriachmada <achmada1998@gmail.com>
2023-05-16 10:39:56 +0700

    new

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 
 const bodyParser = require('body-parser');
 const getUsers = require('./app/api/v1/users/getUsers');
+const { connection } = require('./app/api/v1/database/database');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -13,8 +14,10 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-  res.json(['Welcome to Octopoda version: 1 ']);
+app.get('/', async (req, res) => {
+  const { rows: connected } = await (await connection()).query('SELECT NOW()');
+  console.log('connected[0].now', connected[0].now);
+  res.json(['Welcome to Octopoda version: 1 ', connected[0].now]);
 });
 
 app.use('/users', getUsers);
